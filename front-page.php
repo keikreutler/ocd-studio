2df8079003fbc2902ecd7dbf7d2a14a0c13f227c
keikreutler <kei@ourmachine.net>
2017-01-19 16:40:22 +0100

    Added new vimeo embed hide

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <section id="content" role="main">
 
-    <iframe class="vimeoXtra" src="https://player.vimeo.com/video/200170551?autoplay=1&loop=1&title=0&byline=0&portrait=0" frameborder="0"></iframe>
+    <iframe class="vimeoXtra" src="https://player.vimeo.com/video/200170551?autoplay=1&loop=1&title=0&byline=0&portrait=0&background=1" frameborder="0"></iframe>
 
     <div id="nav">
     	<h1>
